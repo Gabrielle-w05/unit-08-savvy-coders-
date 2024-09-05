736e7f711b3d7373dbfddabb1bc7cf22c6745e62
Gabrielle-w05 <gabriellewestbrook05@gmail.com>
2024-09-05 01:28:45 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let firstName ='Gabrielle';
+let age = 18; 
+let canvote = true;
+let isProgrammer = true;
+let description = 'Gabrielle; 18; is Programer';
+console.log('isProgrammer')
+if (age >= 18) {
+  console.log('You are eligible to vote.');
+} else {
+  console.log('You are not eligible to vote yet.');
+}
+
+let num1 = 5;
+let num2 = 20;
+let sum = num1 + num2;
+
+ 
+let option = 1 + "";
+console.log(typeof option)
+let decision = 2 + "";
+console.log(typeof decision)
+
+if (option) {
+    console.log('valid option')
+} else { 
+    console.log('invalid option')
+}
+
+let message = (age >= 18) ? 'isAdult' : 'Is not Adult';
+
+console.log(message)
+console.log(description)
